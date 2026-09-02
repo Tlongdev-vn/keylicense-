86289ea215cdefaf865c99adbdc51f60894cf0c0
Tlongdev-vn <thienlong4712@gmail.com>
2026-09-02 20:20:49 +0700

    Update MIRANDA-HUB.lua

--- MIRANDA-HUB.lua
+++ MIRANDA-HUB.lua
@@ -5,7 +5,7 @@
 local DOMAIN_VERCEL = "https://keylicensenew2.vercel.app/"
 local DISCORD_INVITE = "https://discord.gg/TvwRC4tba"
 local DISCORD_ICON_URL = "rbxassetid://99761773347476"
-local SAVE_FILE_NAME = "TLongSystem_KeySave.txt" -- File lưu trạng thái key
+local SAVE_FILE_NAME = "TLongHub_Key.txt" -- File lưu trạng thái key
 
 local TweenService = game:GetService("TweenService")
 local RunService = game:GetService("RunService")
